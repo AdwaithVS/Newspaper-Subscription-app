32f741870540c47f4d99352c6e7c767096bceb09
AdwaithVS <adwaithvs512@gmail.com>
2024-07-03 19:47:17 +0530

    Completed

--- src/components/DeleteSub.jsx
+++ src/components/DeleteSub.jsx
@@ -4,7 +4,7 @@ import NavBar from './NavBar'
 const DeleteSub = () => {
     return (
         <div>
-            <NavBar/>
+            <NavBar />
             <div className="container">
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
